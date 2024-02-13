44774e698870518c5f523df8f446c460ca47d874
Donovan McKinney <mckidonno@outlook.com>
2024-02-13 10:52:38 -0600

    Break 2

--- Lecture04/Lab3.py
+++ Lecture04/Lab3.py
@@ -5,3 +5,23 @@
 
 from helper import get_int
 
+class GroceryItem:
+    def __init__(self, name, category, quantity):
+        self.name = name
+        self.category = category
+        self.quantity = quantity
+        
+    def __str__(self):
+        return f"{self.name} - {self.category}: {self.quantity}"
+    
+items = [
+    GroceryItem("Ruffle", "Chips", 3),
+    GroceryItem("Bacon", "Meat", 1),
+    GroceryItem("Ice Cream", "Dairy", 2), 
+    GroceryItem("Coke", "Drink", 12)
+]
+
+x = 0
+for x in items:
+    print(items(1))
+    x += 1
