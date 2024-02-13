e2514375d24597ec9bdca09bf722fe9714a99957
Donovan McKinney <mckidonno@outlook.com>
2024-02-13 11:17:30 -0600

    Lab 3 Ended

--- Lecture04/Lab3.py
+++ Lecture04/Lab3.py
@@ -21,7 +21,24 @@ items = [
     GroceryItem("Coke", "Drink", 12)
 ]
 
-x = 0
-for x in items:
-    print(items(1))
-    x += 1
+def list_item():
+    i = 0
+    for item in items:
+        print(f"Item {i + 1}: {item}")
+        i += 1
+        
+def update_item():
+    list_item()
+    index = get_int(f"\tWhat item would you like to update: ") -1
+    items[index].quantity =get_int(f"How many {items[index].name} do you want: ")
+    list_item()
+    
+while True:
+    print(f"\n1. List items\n2. Update an item\n3. Exit")
+    choice = get_int(f"\nSelection: ")
+    if choice == 1:
+        list_item()
+    elif choice == 2:
+        update_item()
+    else:
+        break
